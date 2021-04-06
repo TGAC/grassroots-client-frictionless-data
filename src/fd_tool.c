febfa9c39ca0e52fa33fc0d5952a768cdf4d0d03
Simon Tyrrell <tyrrell.simon@gmail.com>
2021-04-06 21:51:37 +0100

    added basic readme

--- src/fd_tool.c
+++ src/fd_tool.c
@@ -32,11 +32,23 @@
 #define ALLOCATE_FD_UTIL_TAGS (1)
 #include "frictionless_data_util.h"
 
-const char *GetJSONString (const json_t *json_p, const char * const key_s);
+
+/*
+ * static declarations
+ */
+
+static const char *GetJSONString (const json_t *json_p, const char * const key_s);
+
+static bool CreateCSVFile (const char *filename_s, const char *col_sep_s, const char *row_sep_s, const json_t *headers_p, const json_t *data_p);
+
+static const char *GetChildJSONString (const json_t *entry_p, const char * const key_s);
+
+static int PrintJSONObject (FILE *out_f, const json_t * const json_p, const char * const prefix_s);
 
 
-bool CreateCSVFile (const char *filename_s, const char *col_sep_s, const char *row_sep_s, const json_t *headers_p, const json_t *data_p);
-
+/*
+ * api definitions
+ */
 
 int main (int argc, char *argv [])
 {
@@ -111,8 +123,32 @@ int main (int argc, char *argv [])
 }
 
 
+/*
+ * static definitions
+ */
 
-bool CreateCSVFile (const char *filename_s, const char *col_sep_s, const char *row_sep_s, const json_t *headers_p, const json_t *data_p)
+static const char *GetChildJSONString (const json_t *entry_p, const char * const key_s)
+{
+	const char *value_s = NULL;
+	const json_t *header_name_p = json_object_get (entry_p, key_s);
+
+	if (header_name_p)
+		{
+			if (json_is_string (header_name_p))
+				{
+					value_s = json_string_value (header_name_p);
+				}
+			else
+				{
+					PrintJSONObject (stderr, entry_p, "header for %s is not a string");
+				}
+		}
+
+	return value_s;
+}
+
+
+static bool CreateCSVFile (const char *filename_s, const char *col_sep_s, const char *row_sep_s, const json_t *headers_p, const json_t *data_p)
 {
 	bool success_flag = false;
 	FILE *csv_f = fopen (filename_s, "w");
@@ -133,38 +169,29 @@ bool CreateCSVFile (const char *filename_s, const char *col_sep_s, const char *r
 					for (i = 0; i < num_columns; ++ i)
 						{
 							const json_t *header_p = json_array_get (headers_p, i);
-							const json_t *header_name_p = json_object_get (header_p, FD_TABLE_FIELD_NAME);
+							const char *header_s = GetChildJSONString (header_p, FD_TABLE_FIELD_NAME);
 
-							if (header_name_p)
+							if (header_s)
 								{
-									if (json_is_string (header_name_p))
+									const char *sep_s;
+
+									if (i == num_columns - 1)
 										{
-											const char *header_s = json_string_value (header_name_p);
-
-											if (header_s)
-												{
-													const char *sep_s;
-
-													if (i == num_columns - 1)
-														{
-															sep_s = row_sep_s;
-														}
-													else
-														{
-															sep_s = col_sep_s;
-														}
-
-													fprintf (csv_f, "\"%s\"%s ", header_s, sep_s);
-												}		/* if (header_s) */
-											else
-												{
-													success_flag = false;
-												}
+											sep_s = row_sep_s;
 										}
+									else
+										{
+											sep_s = col_sep_s;
+										}
+
+									fprintf (csv_f, "\"%s\"%s ", header_s, sep_s);
+								}		/* if (header_s) */
+							else
+								{
+									success_flag = false;
 								}
 
 						}		/* for (i = 0; i < num_columns; ++ i) */
-
 
 					/*
 					 * write the data in the same order as the headers
@@ -182,68 +209,63 @@ bool CreateCSVFile (const char *filename_s, const char *col_sep_s, const char *r
 									for (j = 0; j < num_columns; ++ j)
 										{
 											const json_t *header_p = json_array_get (headers_p, j);
-											const json_t *header_name_p = json_object_get (header_p, FD_TABLE_FIELD_NAME);
+											const char *header_s = GetChildJSONString (header_p, FD_TABLE_FIELD_NAME);
 
-											if (header_name_p)
+											if (header_s)
 												{
-													if (json_is_string (header_name_p))
+													const json_t *value_p = json_object_get (row_p, header_s);
+
+													if (value_p)
 														{
-															const char *header_s = json_string_value (header_name_p);
-
-															if (header_s)
+															if (add_sep_flag)
 																{
-																	const json_t *value_p = json_object_get (row_p, header_s);
-
-																	if (value_p)
-																		{
-																			const char *sep_s;
-
-																			if (add_sep_flag)
-																				{
-																					fprintf (csv_f, "%s ", col_sep_s);
-																				}
-																			else
-																				{
-																					add_sep_flag = true;
-																				}
-
-																			if (json_is_string (value_p))
-																				{
-																					const char *value_s = json_string_value (value_p);
-
-																					if (!value_s)
-																						{
-																							value_s = "";
-																						}
-
-																					fprintf (csv_f, "\"%s\"", value_s);
-																				}
-																			else if (json_is_integer (value_p))
-																				{
-																					const json_int_t value = json_integer_value (value_p);
-
-																					fprintf (csv_f, "%" JSON_INTEGER_FORMAT, value);
-																				}
-																			else if (json_is_real (value_p))
-																				{
-																					const double value = json_real_value (value_p);
-
-																					fprintf (csv_f, "%lf", value);
-																				}
-
-																		}
-
-																}		/* if (header_s) */
+																	fprintf (csv_f, "%s ", col_sep_s);
+																}
 															else
 																{
-																	success_flag = false;
+																	add_sep_flag = true;
+																}
+
+															if (json_is_string (value_p))
+																{
+																	const char *value_s = json_string_value (value_p);
+
+																	if (value_s)
+																		{
+																			fprintf (csv_f, "\"%s\"", value_s);
+																		}
+																	else
+																		{
+																			fprintf (csv_f, " ");
+																		}
+																}
+															else if (json_is_integer (value_p))
+																{
+																	const json_int_t value = json_integer_value (value_p);
+
+																	fprintf (csv_f, "%" JSON_INTEGER_FORMAT, value);
+																}
+															else if (json_is_real (value_p))
+																{
+																	const double value = json_real_value (value_p);
+
+																	fprintf (csv_f, "%lf", value);
+																}
+															else
+																{
+																	PrintJSONObject (stderr, value_p, "Unknown JSON type: ");
 																}
 														}
+
+												}		/* if (header_s) */
+											else
+												{
+													success_flag = false;
 												}
 
 										}		/* for (i = 0; i < num_columns; ++ i) */
 
-									fprintf (csv_f, "%s ", row_sep_s);
+									fprintf (csv_f, "%s", row_sep_s);
 								}
 
 						}
@@ -256,16 +278,19 @@ bool CreateCSVFile (const char *filename_s, const char *col_sep_s, const char *r
 					 */
 				}
 
-
 			fclose (csv_f);
 		}		/* if (csv_f) */
+	else
+		{
+			fprintf (stderr, "Failed to open CSV output file \"%s\"\n", filename_s);
+		}
 
 	return success_flag;
 }
 
 
 
-const char *GetJSONString (const json_t *json_p, const char * const key_s)
+static const char *GetJSONString (const json_t *json_p, const char * const key_s)
 {
 	const char *value_s = NULL;
 	json_t *value_p = json_object_get (json_p, key_s);
@@ -280,5 +305,41 @@ const char *GetJSONString (const json_t *json_p, const char * const key_s)
 
 	return value_s;
 }
+
+
+static int PrintJSONObject (FILE *out_f, const json_t * const json_p, const char * const prefix_s)
+{
+	int result = 0;
+	char *json_s = NULL;
+
+	if (prefix_s)
+		{
+			fprintf (out_f, "%s", prefix_s);
+		}
+
+	if (json_p)
+		{
+			json_s = json_dumps (json_p, JSON_INDENT (2));
+
+			if (json_s)
+				{
+					fprintf (out_f, "%s\n", json_s);
+					free (json_s);
+				}
+			else
+				{
+					fprintf (out_f, "ERROR DUMPING!!\n");
+					result = -1;
+				}
+		}
+	else
+		{
+			fprintf (out_f, "NULL JSON OBJECT!!\n");
+		}
+
+
+	return result;
+}
+
 
 
